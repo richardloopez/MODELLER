e96975746bfd0116137ba65b66a9b74e6fbdce98
López-Corbalán_Richard <richardlopezcorbalan@gmail.com>
2025-10-23 10:54:53 +0200

    Add files via upload

---
+++ modeller_code/modeller_lanzador.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=CON_MODELLER_JOB
+#SBATCH --output=salida_slurm_%j.out
+#SBATCH --error=error_slurm_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48  ###################
+
+module load Python/3.11.3-GCCcore-12.3.0
+
+export MODELLER_CORES=$SLURM_CPUS_PER_TASK
+
+srun --cpus-per-task=$SLURM_CPUS_PER_TASK python3 controller.py > salida.out
+
